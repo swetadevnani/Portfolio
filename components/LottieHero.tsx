fafddf0e3dfc2e26248cb35bfa3a7a393faaab6b
Sweta Devnani <swetaux@gmail.com>
2026-04-13 21:14:31 -0400

    Minor tweak

--- components/LottieHero.tsx
+++ components/LottieHero.tsx
@@ -14,14 +14,14 @@ export default function LottieHero() {
   }, [])
 
   return (
-    <div style={{ position: 'absolute', bottom: '40px', left: '-120px', right: '-120px', display: 'flex', justifyContent: 'center' }}>
+    <div style={{ position: 'absolute', bottom: '40px', left: '-120px', right: '-120px', display: 'flex', justifyContent: 'center', opacity: 0.90 }}>
       {/* @ts-ignore */}
       <dotlottie-wc
-        src="https://lottie.host/6f17203c-80c6-4d14-aba5-0193d6ced139/GdQWSddHlf.lottie"
+        src="https://lottie.host/28a9409c-96b1-4e05-bf5b-a50d63dd57ea/StSPSg985Q.lottie"
         style={{
           width: '630px',
           height: '630px',
-WebkitMaskImage: 'linear-gradient(to right, transparent 0%, black 18%, black 82%, transparent 100%)',
+          WebkitMaskImage: 'linear-gradient(to right, transparent 0%, black 18%, black 82%, transparent 100%)',
           maskImage: 'linear-gradient(to right, transparent 0%, black 18%, black 82%, transparent 100%)',
         }}
         speed="0.6"
